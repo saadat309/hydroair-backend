bd0b14354b9d98774b235a79fed413edaed154ac
Saadat Yaseen <saadatyaseen309@gmail.com>
2026-02-28 00:15:09 +0500

    fix: update SMTP port and secure settings to use env functions

--- config/plugins.js
+++ config/plugins.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
       provider: "nodemailer",
       providerOptions: {
         host: env("SMTP_HOST", "smtp.hostinger.com"),
-        port: env.int("SMTP_PORT", 465),
-        secure: env.bool("SMTP_SECURE", true),
+        port: env("SMTP_PORT", "465"),
+        secure: env("SMTP_SECURE", "true"),
         auth: {
           user: env("SMTP_USER"),
           pass: env("SMTP_PASS"),
